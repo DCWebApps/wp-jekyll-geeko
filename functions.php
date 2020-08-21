6476bab5df0d4cef1d9168ca8b69aa3a6ae55e3b
Vinzenz Vietzke <vinz@vinzv.de>
2020-08-22 00:48:41 +0200

    include newer bootrap (css/js) and fontawesome

--- functions.php
+++ functions.php
@@ -73,6 +73,14 @@ function wp_jekyll_setup() {
 		'default-image' => '',
 	) ) );
 
+	// Making this Walker the Default Walker for Nav Menus
+    function prefix_modify_nav_menu_args( $args ) {
+        return array_merge( $args, array(
+            'walker' => new WP_Bootstrap_Navwalker(),
+        ) );
+    }
+    add_filter( 'wp_nav_menu_args', 'prefix_modify_nav_menu_args' );
+	
 	// Add theme support for selective refresh for widgets.
 	add_theme_support( 'customize-selective-refresh-widgets' );
 }
@@ -116,22 +124,19 @@ add_action( 'widgets_init', 'wp_jekyll_widgets_init' );
  */
 function wp_jekyll_scripts() {
         
-        wp_enqueue_style( 'wp-jekyll-fontawesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' );    
-        wp_enqueue_style( 'wp-jekyll-bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' );
+        wp_enqueue_style( 'wp-jekyll-fontawesome', '/css/font-awesome.min.css' );    
+        wp_enqueue_style( 'wp-jekyll-bootstrap', '/css/bootstrap.min.css' );
         
         wp_enqueue_style( 'wp-jekyll-style-main', get_stylesheet_directory_uri() . '/css/main.css' );
         wp_enqueue_style( 'wp-jekyll-style-highlights', get_stylesheet_directory_uri() . '/css/pygment_highlights.css' );
         wp_enqueue_style( 'wp-jekyll-style-fonts', 'https://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,400italic,600,700,800,900' );
         wp_enqueue_style( 'wp-jekyll-style-fonts-2', 'https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' );        
 
-	wp_enqueue_script( 'wp-jekyll-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
-        
+        wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array( 'jquery' ),'4.5.2', true);
         wp_enqueue_script( 'wp-jekyll-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
-
-	wp_enqueue_script( 'wp-jekyll-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
-	
-	wp_enqueue_script( 'wp-jekyll-main', get_template_directory_uri() . '/js/main.js', array(), '20151215', true );
-	wp_enqueue_script( 'wp-jekyll-jquery', get_template_directory_uri() . '/js/jquery-3.5.1.min.js', array(), '20151215', true );
+        wp_enqueue_script( 'wp-jekyll-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+        wp_enqueue_script( 'wp-jekyll-main', get_template_directory_uri() . '/js/main.js', array(), '20151215', true );
+        wp_enqueue_script( 'wp-jekyll-jquery', get_template_directory_uri() . '/js/jquery-3.5.1.min.js', array(), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
