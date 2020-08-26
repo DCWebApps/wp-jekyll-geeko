dae82b5a4273c1c642bfed76a388f0d68c1f1110
Vinzenz Vietzke <vinz@vinzv.de>
2020-08-27 00:09:00 +0200

    edit start page excerpt

--- template-parts/content.php
+++ template-parts/content.php
@@ -11,6 +11,6 @@
     <article class="post-preview">
         <?php the_title( '<h2 class="post-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '<h3></h3></a></h2>' ); ?>
         <p class="post-meta">Posted on <?php the_date( 'F j, Y', 'Posted on: ', '', true ); ?></p>
-        <div class="post-entry"><?php the_content() ?></div>
+        <div class="post-entry"><?php the_excerpt() ?></div>
     </article>
 
